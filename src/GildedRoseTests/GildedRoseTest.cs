b314c2e8e558789c09f6fa3d81fd00da52f89f90
Joseph Mortleman <joseph@mortleman.com>
2024-08-13 12:57:00 +0100

    Add some more granular tests from spec

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -1,6 +1,7 @@
 ﻿using Xunit;
 using System.Collections.Generic;
 using GildedRoseKata;
+using Xunit.Abstractions;
 
 namespace GildedRoseTests
 {
@@ -13,6 +14,83 @@ namespace GildedRoseTests
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.Equal("foo", Items[0].Name);
+        }
+
+        [Fact]
+        public void QualityIsNeverNegative()
+        {
+            var items = new List<Item> { new Item { Quality = 1 } };
+            GildedRose app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.Equal(0, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.Equal(0, items[0].Quality);
+        }
+
+        [Fact]
+        public void QualityIsNeverMoreThan50()
+        {
+            var items = new List<Item> { new Item { Name="Aged Brie", Quality = 49 } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.Equal(50, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.Equal(50, items[0].Quality);
+        }
+
+        [Fact]
+        public void AgedBrieIncreasesInQuality()
+        {
+            var initialQuality = 1;
+            var items = new List<Item> { new Item { Name = "Aged Brie", Quality = initialQuality } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.True(items[0].Quality > initialQuality);
+        }
+
+        [Fact]
+        public void SulfurasNeverDecreasesInQuality()
+        {
+            var initialQuality = 5;
+            var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", Quality = initialQuality } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.Equal(initialQuality, items[0].Quality);
+        }
+
+        [Theory]
+        [InlineData(11, 5, 6)] // Quality increases by 1 when SellIn > 10
+        [InlineData(10, 5, 7)] // Quality increases by 2 when SellIn <= 10
+        [InlineData(9, 5, 7)]
+        [InlineData(8, 5, 7)]
+        [InlineData(7, 5, 7)]
+        [InlineData(6, 5, 7)]
+        [InlineData(5, 5, 8)] // Quality increases by 3 when SellIn <= 5
+        [InlineData(4, 5, 8)]
+        [InlineData(3, 5, 8)]
+        [InlineData(2, 5, 8)]
+        [InlineData(1, 5, 8)]
+        [InlineData(0, 5, 0)] // Quality set to 0 when SellIn <= 0
+        [InlineData(-1, 5, 0)]
+        public void BackstagePassesQualityChanges(int initialSellIn, int initialQuality, int expectedQuality)
+        {
+            var items = new List<Item> {
+                new Item {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Quality = initialQuality,
+                    SellIn = initialSellIn
+                }
+            };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.Equal(expectedQuality, items[0].Quality);
         }
     }
 }
